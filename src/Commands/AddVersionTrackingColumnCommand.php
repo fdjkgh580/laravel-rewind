ccc5ed597bcfc8d889ecc86b2f76e696bd0302f4
jared.cannon <jaredcannon9@gmail.com>
2025-01-12 03:28:53 -0600

    Add version tracking command and stub

---
+++ src/Commands/AddVersionTrackingColumnCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AvocetShores\LaravelRewind\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
+
+class AddVersionTrackingColumnCommand extends Command
+{
+    protected $signature = 'rewind:add-version {table? : The name of the table to add current_version column to}';
+
+    protected $description = 'Generate a migration to add the current_version column to a given table.';
+
+    protected string $stubPath = __DIR__.'/../../stubs/add_current_version_column.stub';
+
+    public function handle()
+    {
+        $table = $this->argument('table');
+
+        // If no table name provided, prompt user
+        if (! $table) {
+            $table = $this->ask('Which table do you want to add the current_version column to?');
+        }
+
+        if (! $table) {
+            $this->error('A table name is required.');
+            return 1;
+        }
+
+        // Determine the class name for the migration
+        $className = 'AddCurrentVersionTo'.Str::studly($table).'Table';
+
+        // Build the filename
+        // e.g. 2023_01_01_000000_add_current_version_to_posts_table.php
+        $timestamp = date('Y_m_d_His');
+        $fileName = $timestamp.'_add_current_version_to_'.$table.'_table.php';
+        $fullPath = database_path('migrations/'.$fileName);
+
+        // Read the stub and replace placeholders
+        $stubContent = (new Filesystem)->get($this->stubPath);
+
+        $stubContent = str_replace(
+            ['DummyClass', 'DummyTable'],
+            [$className, $table],
+            $stubContent
+        );
+
+        // Write the final migration file
+        (new Filesystem)->put($fullPath, $stubContent);
+
+        $this->info("Migration created: {$fileName}");
+        $this->info("Don't forget to run 'php artisan migrate'!");
+
+        return 0;
+    }
+}
--- src/LaravelRewindServiceProvider.php
+++ src/LaravelRewindServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace AvocetShores\LaravelRewind;
 
+use AvocetShores\LaravelRewind\Commands\AddVersionTrackingColumnCommand;
 use AvocetShores\LaravelRewind\Commands\LaravelRewindCommand;
+use AvocetShores\LaravelRewind\Exceptions\InvalidConfigurationException;
+use AvocetShores\LaravelRewind\Models\RewindRevision;
+use Illuminate\Database\Eloquent\Model;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -19,6 +23,30 @@ class LaravelRewindServiceProvider extends PackageServiceProvider
             ->name('laravel-rewind')
             ->hasConfigFile()
             ->hasMigration('create_rewind_revisions_table')
-            ->hasCommand(LaravelRewindCommand::class);
+            ->hasCommand(AddVersionTrackingColumnCommand::class);
+    }
+
+    /**
+     * @throws InvalidConfigurationException
+     */
+    public static function determineRewindRevisionModel(): string
+    {
+        $rewindModel = config('laravel-rewind.rewind_revision_model') ?? RewindRevision::class;
+
+        if (! is_a($rewindModel, Model::class, true)) {
+            throw InvalidConfigurationException::modelIsNotValid($rewindModel);
+        }
+
+        return $rewindModel;
+    }
+
+    /**
+     * @throws InvalidConfigurationException
+     */
+    public static function getRewindRevisionModelInstance(): Model
+    {
+        $rewindModel = static::determineRewindRevisionModel();
+
+        return new $rewindModel();
     }
 }
