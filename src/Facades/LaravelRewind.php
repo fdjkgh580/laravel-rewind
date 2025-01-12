5c3f914599ef37d7d7cb0d34a1853d51ce4c378e
jared.cannon <jaredcannon9@gmail.com>
2025-01-12 15:31:57 -0600

    Add correct facade accessor and add type hinted properties

--- src/Facades/LaravelRewind.php
+++ src/Facades/LaravelRewind.php
@@ -2,15 +2,20 @@
 
 namespace AvocetShores\LaravelRewind\Facades;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @see \AvocetShores\LaravelRewind\LaravelRewind
+ * @see \AvocetShores\LaravelRewind\Services\RewindManager
+ *
+ * @method static bool undo(Model $model)    Undo the most recent change
+ * @method static bool redo(Model $model)    Redo the next version
+ * @method static bool goToVersion(Model $model, int $version)    Jump to a specific version
  */
 class LaravelRewind extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
-        return \AvocetShores\LaravelRewind\LaravelRewind::class;
+        return 'laravel-rewind-manager';
     }
 }
