0b123399116c3588c8e188feed8db3d2fc2f9366
jared.cannon <jaredcannon9@gmail.com>
2025-01-12 03:29:04 -0600

    Add invalid configuration exception

--- src/Exceptions/InvalidConfigurationException.php
+++ src/Exceptions/InvalidConfigurationException.php
@@ -2,7 +2,12 @@
 
 namespace AvocetShores\LaravelRewind\Exceptions;
 
-class InvalidConfigurationException
-{
+use AvocetShores\LaravelRewind\Exceptions\LaravelRewindException;
 
+class InvalidConfigurationException extends LaravelRewindException
+{
+    public static function ModelIsNotValid($model): static
+    {
+        return new static("The model [{$model}] isn't a valid Eloquent model.");
+    }
 }
